1eb89798a728f14dabee34019272228428afdc2b
ANWDM <pv4003337@gmail.com>
2023-06-09 16:42:57 +0530

    data fetching thigs implemented

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { View , StyleSheet } from "react-native";
 import { ActivityIndicator } from "react-native";
 import * as Location from 'expo-location'
+import {WHEATHER_API_KEY} from '@env'
 
 const Tab = createBottomTabNavigator()
 
@@ -19,13 +20,30 @@ const Home  = () => {
         return
       }
       let location = await Location.getCurrentPositionAsync({})
-      setLocation(location)
+      setLocation(location)    
     })
   },[])
 
   const [loading , setLoading] = useState(true)
   const [location , setLocation] = useState(null)
   const [err , setErr] = useState(null)
+  const [wheatherData , setWheatherData] = useState([])
+  const [lat , setLat] = useState([])
+  const [lon , setLon] = useState([])
+
+  const fetchData = async () => {
+    try {
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${WHEATHER_API_KEY}`)
+    const data = await res.json()
+    setWheatherData(data)
+    setLoading(false)
+    } catch (error) {
+      setErr('Could not fetch weather')
+    } finally {
+      setLoading(false)
+    }
+    
+  }
 
 
   if(loading) {
@@ -34,6 +52,10 @@ const Home  = () => {
         <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
+  }
+
+  if(location) {
+    console.log(location)
   }
   
 
