5d2764a92f7071068a51a1ffd25938cc857c9c56
ANWDM <pv4003337@gmail.com>
2023-06-09 16:14:07 +0530

    some changes

--- App.js
+++ App.js
@@ -1,31 +1,48 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Tabs from "./src/components/Tabs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View , StyleSheet } from "react-native";
 import { ActivityIndicator } from "react-native";
+import * as Location from 'expo-location'
 
 const Tab = createBottomTabNavigator()
 
 
 const Home  = () => {
 
+  useEffect(() => {
+    (async () => {
+      let {status} = await loading.requestForeroundPermissionsAsync()
+      if(status !== 'granted'){
+        setErr("permission to access location was denied")
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })
+  },[])
+
   const [loading , setLoading] = useState(true)
+  const [location , setLocation] = useState(null)
+  const [err , setErr] = useState(null)
+
 
   if(loading) {
     return (
-      <View style={}>
+      <View style={styles.container}>
         <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
   }
+  
 
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   
-}
+)}
 
 const styles = StyleSheet.create({
   container:{
