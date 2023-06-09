4a046ffd8cf09edbee7f2939e206f873841f87e8
ANWDM <pv4003337@gmail.com>
2023-06-09 12:39:04 +0530

    adding navigation in the app

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import react from "react";
 import { View , StyleSheet } from "react-native";
 import CheckWheather from "./src/components/CheckWheather";
 import UpcomingWheather from "./src/components/UpcomingWeather";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import City from "./src/components/City";
+import {Feather} from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator()
 
 const Home  = () => {
   return (
-   <View style={styles.container}>
-    <UpcomingWheather />
-   </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CheckWheather} />
+        <Tab.Screen name={'Upcoming'} component={UpcomingWheather} />
+        <Tab.Screen name={"City"} component={City} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
